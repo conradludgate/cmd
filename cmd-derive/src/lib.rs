844fb31e56d92144a7e9d5bc478f58e601814b4a
Conrad Ludgate <conradludgate@gmail.com>
2020-05-08 16:56:34 +0100

    feat: parse simple inputs

--- cmd-derive/src/lib.rs
+++ cmd-derive/src/lib.rs
@@ -5,20 +5,36 @@ use proc_macro2::{TokenStream as Stream, Span};
 use proc_macro_hack::proc_macro_hack;
 use quote::{quote, ToTokens};
 use syn::{parse_macro_input, Expr, Ident};
+use syn::parse::{Parse, ParseStream, Result};
+use syn::punctuated::Punctuated;
+use syn::Token;
+
+// fn str_expr(s: &str) -> Expr {
+//     Expr::Lit(
+//         syn::ExprLit {
+//             attrs: vec![],
+//             lit: syn::Lit::Str(
+//                 syn::LitStr::new(s, Span::call_site())
+//             ),
+//         }
+//     )
+// }
+
+fn str_expr(s: &str, span: Span) -> Expr {
+    Expr::Lit(
+        syn::ExprLit {
+            attrs: vec![],
+            lit: syn::Lit::Str(
+                syn::LitStr::new(s, span)
+            ),
+        }
+    )
+}
 
 #[proc_macro_hack]
 pub fn cmd(input: TokenStream) -> TokenStream {
-    // let root = parse_macro_input!(input as Cmd);
+    let root = parse_macro_input!(input as Cmd);
     
-    // input = `ls | grep "Cargo"`
-    let root = Cmd{
-        envs: None,
-        commands: vec![
-            Command {span: Span::call_site(), name: "ls".to_string(), args: vec![]},
-            Command {span: Span::call_site(), name: "grep".to_string(), args: vec!["Cargo".to_string()]},
-        ],
-        stdin: None,
-    };
     TokenStream::from(root.into_token_stream())
 }
 
@@ -30,13 +46,68 @@ struct Cmd {
 
 struct Command {
     span: Span,
-    name: String,
-    args: Vec<String>
+    name: Expr,
+    args: Vec<Expr>
 }
 
-// impl Parse for Cmd {
+impl Parse for Cmd {
+    fn parse(input: ParseStream) -> Result<Self> {
 
-// }
+        let commands = Punctuated::<Command, Token![|]>::parse_terminated(input)?;
+        
+        Ok(Cmd{
+            envs: None,
+            commands: commands.into_iter().collect(),
+            stdin: None
+        })
+    }
+}
+
+fn convert_expr(expr: Expr) -> Expr {
+    match expr {
+        Expr::Path(path) => {
+            match path.path.get_ident() {
+                Some(ident) => str_expr(&format!("{}", ident), ident.span()),
+                None => Expr::Path(path),
+            }
+        },
+        _ => expr,
+    }
+}
+
+impl Parse for Command {
+    fn parse(input: ParseStream) -> Result<Self> {
+        // ls -la => name: "ls", args: vec!["-la"]
+        // grep "Cargo" => name: "grep", args: vec!["Cargo"]
+        // grep #cargo => name: "grep", args: vec![cargo]
+
+        // TODO: Will require lots of span checking
+        // "ls -la" will read in the stream as "ls" "-" "la",
+        // But the span locations will show that "-" is next to "la"
+
+        let name_ident = input.parse::<Ident>()?;
+        let name = str_expr(&format!("{}", name_ident), name_ident.span());
+
+        let mut args = Vec::<Expr>::new();
+        loop {
+            if input.is_empty() || input.peek(Token![|]) {
+                break;
+            }
+
+            let expr = convert_expr(input.parse::<Expr>()?);
+
+            args.push(expr);
+        }
+
+        Ok(Command{
+            span: name_ident.span(),
+            name: name,
+            args: args,
+        })
+
+        // Err(syn::parse::Error::new(Span::call_site(), "not_implemented"))
+    }
+}
 
 impl ToTokens for Cmd {
     fn to_tokens(&self, tokens: &mut Stream) {
